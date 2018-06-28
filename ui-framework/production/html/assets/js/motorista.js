819fa28d32ceee652cba907c20ac7cc0caf86ada
carlos <carlosjleonel@hotmail.com>
2018-06-27 22:45:20 -0300

    Adicionando cadastro e listagem em veiculo e motorista

--- ui-framework/production/html/assets/js/motorista.js
+++ ui-framework/production/html/assets/js/motorista.js
@@ -10,18 +10,62 @@ $("#form-cadastro-motorista").submit(function( event ) {
    $.post( userUrl, { user: { name: name, cpf:cpf, rg:rg} })
    .done(function( dataUser ) {
       $.post( driveUrl, { driver: { user_id: dataUser.id } })
-      .done(function( dataDriver ) {
-         $("#text-alert").html("Motorista cadastrado com sucesso!");
-         $("#alert-form-driver").toggleClass("alert-success").show();
-      })
-      .fail(function() {
-         $("#text-alert").html("Ocorreu algum erro no cadastro!");
-         $("#alert-form-driver").toggleClass("alert-danger").show();
-      });
+        .done(function( dataDriver ) {
+            $("#text-alert").html("Motorista cadastrado com sucesso!");
+            $("#alert-form-driver").toggleClass("alert-success").show();
+
+            $('#dataDrivers').append(`
+                                <tr>
+                                    <td class="v-align-middle">
+                                        <p>${name}</p>
+                                    </td>
+                                    <td class="v-align-middle">
+                                        <p>${cpf}</p>
+                                    </td>
+                                    <td class="v-align-middle">
+                                        <p>${rg}</p>
+                                    </td>
+                                </tr>
+                            `)
+            
+        })
+        .fail(function() {
+            $("#text-alert").html("Ocorreu algum erro no cadastro!");
+            $("#alert-form-driver").toggleClass("alert-danger").show();
+        });
    })
    .fail(function() {
       $("#text-alert").html("Ocorreu algum erro no cadastro!");
       $("#alert-form-driver").toggleClass("alert-danger").show();
    });
    $("#form-cadastro-motorista")[0].reset();
+})
+
+$(document).ready(() => {
+    
+    $.ajax({
+        method:"GET",
+        url:baseUrl+"drivers",
+        success: (result) => {
+            result.map(driver => {
+                
+                $('#dataDrivers').append(`
+                                <tr>
+                                    <td class="v-align-middle">
+                                        <p>${driver.user.name || "desconhecido"}</p>
+                                    </td>
+                                    <td class="v-align-middle">
+                                        <p>${driver.user.cpf || "desconhecido"}</p>
+                                    </td>
+                                    <td class="v-align-middle">
+                                        <p>${driver.user.rg || "desconhecido"}</p>
+                                    </td>
+                                </tr>
+                           `)
+            })
+        },
+        error: (err) =>{
+            console.log(err)
+        }
+    })
 })
--- ui-framework/production/html/assets/js/veiculo.js
+++ ui-framework/production/html/assets/js/veiculo.js
@@ -1,16 +1,71 @@
 let baseUrl = "https://epturc-levo.herokuapp.com/api/v1/"
 $("#form-cadastro-veiculo").submit(function( event ) {
    let vehiclesUrl = baseUrl + "vehicles"
+
+   let licensePlate = $("#license-plate").val().toString()
    let companyId = $("#company-id").val().toString()
+   let colorVehicle = $("#color-vehicle").val().toString()
+   let modelVehicle = $("#model-vehicle").val().toString()
+
    event.preventDefault();
-   $.post( vehiclesUrl, { vehicle: { company_id : companyId } })
-   .done(function( dataUser ) {
+   $.post( vehiclesUrl, { vehicle: { company_id : companyId,license_plate : licensePlate, model: modelVehicle, color: colorVehicle} })
+   .done(function( res ) {
       $("#text-alert").html("Veículo cadastrado com sucesso!");
       $("#alert-form-driver").toggleClass("alert-success").show();
+
+      $('#dataVehicles').append(`
+                                <tr>
+                                    <td class="v-align-middle">
+                                        <p>${ vehicle.placa || "desconhecido"}</p>
+                                    </td>
+                                    <td class="v-align-middle">
+                                        <p>${ vehicle.modelo|| "desconhecido"}</p>
+                                    </td>
+                                    <td class="v-align-middle">
+                                        <p>${ vehicle.companhia || "desconhecido"}</p>
+                                    </td>
+                                    <td class="v-align-middle">
+                                        <p>${ vehicle.cor|| "desconhecido"}</p>
+                                    </td>
+                                </tr>
+                           `)
    })
    .fail(function() {
       $("#text-alert").html("Ocorreu um erro no cadastro!");
       $("#alert-form-driver").toggleClass("alert-danger").show();
    });
    $("#form-cadastro-veiculo")[0].reset();
+})
+
+
+$(document).ready(() => {
+    
+    $.ajax({
+        method:"GET",
+        url:baseUrl+"vehicles",
+        success: (result) => {
+            result.map(vehicle => {
+                console.log(vehicle)
+                $('#dataVehicles').append(`
+                                <tr>
+                                    <td class="v-align-middle">
+                                        <p>${vehicle.license_plate || "desconhecido"}</p>
+                                    </td>
+                                    <td class="v-align-middle">
+                                        <p>${vehicle.model || "desconhecido"}</p>
+                                    </td>
+                                    <td class="v-align-middle">
+                                        <p>${vehicle.company_id|| "desconhecido"}</p>
+                                    </td>
+                                    <td class="v-align-middle">
+                                        <p>${vehicle.color|| "desconhecido"}</p>
+                                    </td>
+                                </tr>
+                           `)
+            })
+        },
+        error: (err) =>{
+            console.log(err)
+        }
+    })
 })
